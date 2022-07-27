bf7610a907671ca5cb79381546d1b6ca47c7eae1
Alex G <agoponenko@gmail.com>
2022-07-27 17:11:22 -0400

    improve the probe

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 #define O_BINARY 0
 #endif
 
+#include "simple_server/client.h"
 
 typedef struct timespec my_time;
 
@@ -30,6 +31,12 @@ double time_diff(my_time start, my_time end)
   int sec = end.tv_sec - start.tv_sec;
   int nsec = end.tv_nsec - start.tv_nsec;
   return (double) sec + 1e-9 * (double) nsec;
+}
+
+
+long time_to_ts(my_time time)
+{
+  return (long) time.tv_sec;
 }
 
 
@@ -51,24 +58,26 @@ int open_stripe_file(const char *tfile, const int mode, const int stripe_offset)
    */
   if (fd < 0) {
     fprintf(stderr,"llapi_file_open failed: Error %d (%s) \n", -fd, strerror(-fd));
-    return -1;
   }
   return fd;
 }
 
 
-void do_write(size_t b_size, size_t fileSize, char *fileContent, int out)
+int do_write(size_t b_size, size_t fileSize, char *fileContent, int out)
 {
   for (char *p = fileContent; p < fileContent + fileSize;)
   {
-    size_t written = write(out, p, b_size);
-    if (written == -1) {
-      // TODO: proper error handling
-      exit(0xff);
+    for (size_t to_write = b_size; to_write > 0;) {
+      size_t written = write(out, p, to_write);
+      if (written == -1) {
+        return -1;
+      }
+      p += written;
+      to_write -= written;
     }
-    p += written;
     fsync(out);
   }
+  return 0;
 }
 
 
@@ -82,64 +91,30 @@ void do_write1(size_t b_size, size_t fileSize, char *fileContent, int out)
 }
 
 
-double testPosixIO(const char* outFile, const size_t ost, const size_t size)
-{
-  size_t b_size = 64 * 1024;
-  size_t s_count = 16 * size;
-  my_time start_t, end_t;
-
-  size_t fileSize = s_count * b_size;
-  // allocate buffer for the file content
-  char *fileContent = malloc(fileSize);
-  if (fileContent == 0) {
-    fprintf(stderr, "Can't allocate buffer of size %zu\n", fileSize);
-    exit(-1);
-  }
-  // fill up the file content
-  int x1 = rand();
-  int x2 = rand();
-  for (int *p = (int*)fileContent; p < (int*)(fileContent+fileSize); p+= 2) {
-    *p = x1;
-    *(p+1) = x2;
-    x1++;
-    x2 += 7905479;
-  }
-  // open the file
-  int out = open_stripe_file(outFile, 0666, ost);
-  if (out < 0)
-  {
-    fprintf(stderr, "Can't open output file %s on OST %zu\n", outFile, ost);
-    exit(-1);
-  }
-
-  // Write content to the file
-  start_t = get_time();
-  do_write(b_size, fileSize, fileContent, out);
-  end_t = get_time();
-  // close the file
-  close(out);
-  unlink(outFile);
-  free(fileContent);
-  return time_diff(start_t, end_t);
-}
-
-
 void usage(FILE *f, char *exe)
 {
-  fprintf(f, "Usage: %s { {-h|--help} | {-f|--filename} <filename> {-o|--ost} <OST #> [{-s|--size} <MiB>]}\n", exe);
+  fprintf(f, "Usage: %s { {-h|--help} | {-f|--filename} <filename> {-o|--ost} <OST #>\n"
+             "             [{-b|--block_size <KiB>] [{-s|--segments <count>] [{-t|--tries <count>]}\n", exe);
 }
 
 
 int main(int argc, char* argv[])
 {
+  /////////////////////////////////////////////////////////////
+  // parse options
   char filename[256] = { 0 };
   size_t ost = SIZE_MAX;
-  size_t size = 1;
+  size_t b_size = 64 * 1024;
+  size_t s_count = 16;
+  size_t n_tries = 5;
+
   struct option longopts[] = {
       { "help", no_argument, NULL, 'h' },
       { "filename", required_argument, NULL, 'f' },
       { "ost", required_argument, NULL, 'o' },
-      { "size", required_argument, NULL, 's' },
+      { "segments", required_argument, NULL, 's' },
+      { "block_size", required_argument, NULL, 'b' },
+      { "tries", required_argument, NULL, 't' },
       { 0 }
   };
 
@@ -153,7 +128,7 @@ int main(int argc, char* argv[])
      * one colon after an option indicates that it has an argument, two
      * indicates that the argument is optional. order is unimportant.
      */
-    int opt = getopt_long (argc, argv, "ho:f:s:", longopts, 0);
+    int opt = getopt_long (argc, argv, "ho:f:s:b:t:", longopts, 0);
 
     if (opt == -1) {
       /* a return value of -1 indicates that there are no more options */
@@ -186,9 +161,25 @@ int main(int argc, char* argv[])
         }
         break;
       case 's':
-        size = strtoul(optarg, &pEnd, 10);
-        if (size == 0 || pEnd != optarg + strlen(optarg)) {
-          fprintf(stderr, "Error: bad value for size in MiB (positive integer is expected): %s\n", optarg);
+        s_count = strtoul(optarg, &pEnd, 10);
+        if (s_count == 0 || pEnd != optarg + strlen(optarg)) {
+          fprintf(stderr, "Error: bad value for number of segments (positive integer is expected): %s\n", optarg);
+          usage (stderr, argv[0]);
+          return 1;
+        }
+        break;
+      case 'b':
+        b_size = 1024 * strtoul(optarg, &pEnd, 10);
+        if (b_size == 0 || pEnd != optarg + strlen(optarg)) {
+          fprintf(stderr, "Error: bad value for block size (positive integer is expected): %s\n", optarg);
+          usage (stderr, argv[0]);
+          return 1;
+        }
+        break;
+      case 't':
+        n_tries = strtoul(optarg, &pEnd, 10);
+        if (n_tries == 0 || pEnd != optarg + strlen(optarg)) {
+          fprintf(stderr, "Error: bad value for number of tries (positive integer is expected): %s\n", optarg);
           usage (stderr, argv[0]);
           return 1;
         }
@@ -219,11 +210,57 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  time_t start = time(NULL);
-  double duration = testPosixIO(filename, ost, size);
-  time_t end = time(NULL);
 
-  printf("%ld, %ld, %ld, %f, %ld\n", start, end, ost, duration, size);
+  /////////////////////////////////////////////////////////////
+  // Doing the work
+  my_time start_t, end_t;
+  size_t fileSize = s_count * b_size;
+  // allocate buffer for the file content
+  char *fileContent = malloc(fileSize);
+  if (fileContent == 0) {
+    fprintf(stderr, "Can't allocate buffer of size %zu\n", fileSize);
+    return 2;
+  }
+  // fill up the file content
+  int x1 = rand();
+  int x2 = rand();
+  for (int *p = (int*)fileContent; p < (int*)(fileContent+fileSize); p+= 2) {
+    *p = x1;
+    *(p+1) = x2;
+    x1++;
+    x2 += 7905479;
+  }
 
-  return 0;
+  // delete file in case it exists
+  unlink(filename);
+
+  // we repeat several times in case of an error during the write
+  for (size_t i = 0; i < n_tries; i++) {
+    // open the file
+    int out = open_stripe_file(filename, 0666, ost);
+    if (out < 0) {
+      fprintf(stderr, "Can't open output file %s on OST %zu\n", filename, ost);
+      return 3;
+    }
+
+    // Write content to the file
+    start_t = get_time();
+    int result = do_write(b_size, fileSize, fileContent, out);
+    end_t = get_time();
+    // close the file
+    close(out);
+    unlink(filename);
+
+    if (result == 0) {
+      // good experiment - write the result
+      double duration = time_diff(start_t, end_t);
+      printf("%ld, %ld, %zu, %f, %zu\n", time_to_ts(start_t), time_to_ts(end_t), ost, duration, fileSize);
+      free(fileContent);
+      return 0;
+    }
+    fprintf(stderr, "Error writing to the file. Trying again...\n");
+  }
+  free(fileContent);
+  fprintf(stderr, "Tried %zu times but could not write the file without errors. Aborting.\n", n_tries);
+  return 4;
 }
